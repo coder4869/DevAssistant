1629907b7f24d12c2a2406266fb19a456df88803
coder4869 <coder4869@gmail.com>
2022-12-11 17:26:34 +0800

    add license

--- plugins/project/run.py
+++ plugins/project/run.py
@@ -1,18 +1,18 @@
 # coding=UTF-8
 # MIT License
-
+# 
 # Copyright (c) 2021~2022 [coder4869](https://github.com/coder4869)
-
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-
+# 
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,7 +44,7 @@ args = parser.parse_args()
   
 if __name__ == '__main__':
     if args.type == "project":
-        project.Project.create(BASE_DIR + "/DevKits.json")
+        project.Project.create(BASE_DIR + "/project.json")
     elif args.type == "module":
         module.Module.MOUDLE_DIR = "/src-app/"
         module.Module.add(args.root_dir, module.ModuleType(args.module_type), args.module_name, has_main=True)
