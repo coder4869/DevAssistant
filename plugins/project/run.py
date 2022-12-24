043c5ff274aa4d23a79a8f43d3a6064f85b4c788
coder4869 <coder4869@gmail.com>
2022-12-24 11:01:16 +0800

    add json-config by parameter

--- plugins/project/run.py
+++ plugins/project/run.py
@@ -32,20 +32,23 @@ sys.path.append( PYTOOL_DIR )
 from project import project                                                     
 from module import module                                                     
 
-# python3 run.py --type project 
+# python3 run.py --type project [--config ./DevKits.json]
 # python3 run.py --type module --root_dir ../../DevKits --module_type 1 --module_name QtKit
 
 parser = ArgumentParser()
-parser.add_argument('--type', dest='type', type=str, default=['project'], help="gen project or add module to project")    
+parser.add_argument('--type',   dest='type',   type=str, default='project', help="gen project or add module to project")    
+# Only for Project
+parser.add_argument('--config', dest='config', type=str, default=BASE_DIR + "/DevKits.json", help="project config.json")    
 # Only for Module
-parser.add_argument('--root_dir',    dest='root_dir',    type=str, default=['.'], help="project root dir")    
+parser.add_argument('--root_dir',    dest='root_dir',    type=str, default='.', help="project root dir")    
 parser.add_argument('--module_type', dest='module_type', type=int, default=[0], help="ModuleType for Kit or Qt")
 parser.add_argument('--module_name', dest='module_name', type=str, default=['CCoreKit'], help="kit or App name")
 args = parser.parse_args()
   
 if __name__ == '__main__':
     if args.type == "project":
-        project.Project.create(BASE_DIR + "/project.json")
+        print("json = ", args.config)
+        project.Project.create(args.config)
     elif args.type == "module":
         module.Module.MOUDLE_DIR = "/src-app/"
         module.Module.add(args.root_dir, module.ModuleType(args.module_type), args.module_name, has_main=True, deps=[])
