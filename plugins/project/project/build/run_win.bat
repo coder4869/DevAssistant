e1108a0c65b7ad097f62915a1015bf9f3262a2cd
coder4869 <coder4869@gmail.com>
2023-01-04 08:09:50 +0800

    optimize script for win

--- plugins/project/project/build/run_win.bat
+++ plugins/project/project/build/run_win.bat
@@ -1,17 +1,17 @@
 @REM MIT License
-
+@REM 
 @REM Copyright (c) 2021~2022 [coder4869](https://github.com/coder4869)
-
+@REM 
 @REM Permission is hereby granted, free of charge, to any person obtaining a copy
 @REM of this software and associated documentation files (the "Software"), to deal
 @REM in the Software without restriction, including without limitation the rights
 @REM to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 @REM copies of the Software, and to permit persons to whom the Software is
 @REM furnished to do so, subject to the following conditions:
-
+@REM 
 @REM The above copyright notice and this permission notice shall be included in all
 @REM copies or substantial portions of the Software.
-
+@REM 
 @REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 @REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 @REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 @REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 @REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 @REM SOFTWARE.
-
+@REM 
 @echo off
 
 ::set var
@@ -44,6 +44,22 @@ echo PY_INSTALL_DIR = %PY_INSTALL_DIR%
 :: cd %ROOT_DIR%
 if exist %BUILD_DIR% rd /s /q %BUILD_DIR%
 md %BUILD_DIR%
+
+:: clean py cache
+rd /s /q %ROOT_DIR%/../plugins/.DS_Store
+rd /s /q %ROOT_DIR%/../plugins/__pycache__
+
+rd /s /q %ROOT_DIR%/../plugins/*/.DS_Store
+rd /s /q %ROOT_DIR%/../plugins/*/__pycache__
+
+rd /s /q %ROOT_DIR%/../plugins/*/*/.DS_Store
+rd /s /q %ROOT_DIR%/../plugins/*/*/__pycache__
+
+rd /s /q %ROOT_DIR%/../plugins/*/*/*/.DS_Store
+rd /s /q %ROOT_DIR%/../plugins/*/*/*/__pycache__
+
+rd /s /q %ROOT_DIR%/../plugins/*/*/*/*/.DS_Store
+rd /s /q %ROOT_DIR%/../plugins/*/*/*/*/__pycache__
 
 :: run cmake: gen vs-project
 cmake -Wno-dev %ROOT_DIR% -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DPROJECT_NAME=%APP_NAME% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DCMAKE_PREFIX_PATH=%QT_INSTALL_DIR% -DQT_INSTALL_DIR=%QT_INSTALL_DIR% -DPY_INSTALL_DIR=%PY_INSTALL_DIR% -DWIN=ON -H%ROOT_DIR% -B%BUILD_DIR%
