38ca90508d2d6671b478844a831f359314151d08
coder4869@gmial.com <158755love>
2023-09-05 23:55:39 +0800

    添加文件基础功能

---
+++ app/src/kits/CCoreKit/utils/CKFile.cpp
@@ -0,0 +1,66 @@
+﻿// MIT License
+//
+// Copyright (c) 2021~2022 [coder4869](https://github.com/coder4869)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "CKFile.h"
+
+#include <fstream>
+#include <iostream>
+
+namespace CK {
+
+int File::IsFileExist(const std::string& file_path)
+{
+    std::fstream fs;
+    fs.open(file_path.c_str(), std::ios_base::in);
+    if (!fs.is_open()) {
+        std::cout << __FUNCTION__ << ": Open File " + file_path + " Failed" << std::endl;
+        return -1;
+    }
+    fs.close();
+    return 0;
+}
+
+int File::LoadFileString(const std::string& file_path, std::string& out_str)
+{
+    std::fstream fs;
+    fs.open(file_path.c_str(), std::ios_base::in);
+    if (!fs.is_open()) {
+        std::cout << __FUNCTION__ << ": Open File " + file_path + " Failed" << std::endl;
+        return -1;
+    }
+
+    if (fs) {
+        fs.seekg(0, std::ios::end);
+        out_str.resize(fs.tellg());
+        fs.seekg(0, std::ios::beg);
+        fs.read(&out_str[0], out_str.size());
+        fs.close();
+
+        return 0;
+    }
+
+    std::cout << __FUNCTION__ << ": Read File " + file_path + " Failed" << std::endl;
+    fs.close();
+    return -1;
+}
+
+} // namespace CK
