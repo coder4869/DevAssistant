43f3c21c55b8416fd5921706ba2ecccac0d648da
coder4869@gmial.com <158755love>
2023-09-09 20:21:38 +0800

    adjust template lib output dir

--- plugins/project/project/build/run_win.bat
+++ plugins/project/project/build/run_win.bat
@@ -26,7 +26,6 @@
 SET APP_NAME=PROJ_NAME
 SET ROOT_DIR=%~dp0\..\..
 SET BUILD_DIR=%ROOT_DIR%\build_win
-SET BIN_DIR=%ROOT_DIR%\bin64
 :: https://www.cnblogs.com/doudougou/archive/2011/08/22/2148851.html
 @REM REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\devenv.exe"
 SET VS_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
@@ -45,9 +44,9 @@ echo PY_INSTALL_DIR = %PY_INSTALL_DIR%
 
 :: make %BUILD_DIR% and cd %BUILD_DIR% 
 :: cd %ROOT_DIR%
+:: https://blog.csdn.net/xiaoerbuyu1233/article/details/108490963 RMDIR %BUILD_DIR% /S /Q
 if exist %BUILD_DIR% rd /s /q %BUILD_DIR%
 md %BUILD_DIR%
-if exist %BIN_DIR% rd /s /q %BIN_DIR%
 
 :: clean py cache
 SET PLUGIN_DIR=%ROOT_DIR%\..\plugins
@@ -61,7 +60,7 @@ for /f "delims=" %%i in ('dir /ad/b/s "%PLUGIN_DIR%"') do (
 :: -G"Visual Studio 15 2017"
 :: -G"Visual Studio 16 2019"
 :: -G"Visual Studio 17 2022"
-cmake -Wno-dev %ROOT_DIR% -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DPROJECT_NAME=%APP_NAME% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DCMAKE_PREFIX_PATH=%QT_INSTALL_DIR% -DEXECUTABLE_OUTPUT_PATH=%BIN_DIR% -DQT_INSTALL_DIR=%QT_INSTALL_DIR% -DPY_INSTALL_DIR=%PY_INSTALL_DIR% -DWIN=ON -H%ROOT_DIR% -B%BUILD_DIR%
+cmake -Wno-dev %ROOT_DIR% -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DPROJECT_NAME=%APP_NAME% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DCMAKE_PREFIX_PATH=%QT_INSTALL_DIR%  -DQT_INSTALL_DIR=%QT_INSTALL_DIR% -DPY_INSTALL_DIR=%PY_INSTALL_DIR% -DWIN=ON -H%ROOT_DIR% -B%BUILD_DIR%
 if exist %VS_PROJ% start %VS_EXE% %VS_PROJ%
 
 echo. & pause 
