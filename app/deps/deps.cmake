f0915302dbffddd9acd53fe0beae95f78bf11645
coder4869@gmial.com <158755love>
2023-10-15 21:20:26 +0800

    update app third-party

--- app/deps/deps.cmake
+++ app/deps/deps.cmake
@@ -25,13 +25,20 @@ set(INC_FILES  ${INC_FILES}  ${SRC_ROOT})
 message("SRC_ROOT = ${SRC_ROOT}")
 
 set(THIRD_PARTY_INC ${THIRD_PARTY_INC} ${SRC_ROOT}/include)
-set(THIRD_PARTY_LIB ${THIRD_PARTY_LIB} ${SRC_ROOT}/lib/*.lib)
+if(WIN)
+    set(LIB_DIR lib_win_x64)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set(LIB_DIR lib_win_x86)
+    endif()
 
-# QtCryptoKit
-if(QtCryptoKit)
-    set(QtCryptoKit_INC)
-    set(QtCryptoKit_LIB)
-    include(${SRC_ROOT}/QtCryptoKit/QtCryptoKit.cmake)
-    set(THIRD_PARTY_INC ${THIRD_PARTY_INC} ${QtCryptoKit_INC})
-    set(THIRD_PARTY_LIB ${THIRD_PARTY_LIB} ${QtCryptoKit_LIB})
-endif(QtCryptoKit)
+    FILE(GLOB_RECURSE STATIC_LIBS
+        ${SRC_ROOT}/${LIB_DIR}/*.lib
+        )
+    set(THIRD_PARTY_LIB ${THIRD_PARTY_LIB} ${STATIC_LIBS})
+    
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        APP_ADD_RES_RECURSE_PLAIN("${SRC_ROOT}/*.dll" "Debug/")
+    else()
+        APP_ADD_RES_RECURSE_PLAIN("${SRC_ROOT}/*.dll" "Release/")
+    endif()
+endif(WIN)
