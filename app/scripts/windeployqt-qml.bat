092f1d46491eaf35e89aa7417316bf1cc5165897
coder4869@gmial.com <158755love>
2023-10-15 18:47:19 +0800

    windows : add qt-deps deploy bat

---
+++ app/scripts/windeployqt-qml.bat
@@ -0,0 +1,11 @@
+﻿
+set ROOT=%~dp0\..
+
+start /wait %QT_HOME%\bin\windeployqt.exe -qmldir %QT_HOME%/qml %ROOT%\bin64\Release\DevAssistant.exe
+
+@REM Fix "QtQuick.Shapes" load failed issue
+@REM xcopy /S /F /Y %QT_HOME%\bin\Qt5QuickShapes.dll %ROOT%\bin64\Release\Qt5QuickShapes.dll
+@REM MD %ROOT%\bin64\QtQuick\Shapes
+@REM xcopy /S /F /Y %QT_HOME%\qml\QtQuick\Shapes\* %ROOT%\bin64\Release\QtQuick\Shapes\
+
+pause
