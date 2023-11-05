c3520b08dcaa7c4581d7de0d2f2b99ad121294c8
coder4869@gmial.com <158755love>
2023-11-05 13:51:27 +0800

    add timer

--- app/src/kits/CUtils/CUTimer.cpp
+++ app/src/kits/CUtils/CUTimer.cpp
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,11 +20,50 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef CUTILS_H
-#define CUTILS_H
+#include "CUTimer.h"
 
-#include <CUtils/CUString.h>
-#include <CUtils/CUFile.h>
-#include <CUtils/CUTimer.h>
+#include <sys/timeb.h>
+#include <time.h>
 
-#endif /* CUTILS_H */
+#include <iostream>
+
+#pragma optimize("", off)
+
+NS_CU_BEGIN
+
+uint64_t Timer::GetTimeSecond()
+{
+	timeb t;
+	ftime(&t); // Get ms time
+	return t.time;
+}
+
+/// YYYY-MM-DD HH:mm:ss		
+const char* Timer::SecondTime2Str(uint64_t timestamp, const char* format)
+{
+	time_t tim = (time_t)timestamp;
+	struct tm t = *localtime(&tim);
+	char tim_str[100];
+	std::string tim_fmt;
+	if (format == "YYYYMMDD-HHmmss") {
+		strftime(tim_str, sizeof(tim_str), "%Y%m%d-%H%M%S", &t);
+		tim_fmt = tim_str;
+	}
+	else {
+		// format == "YYYY-MM-DD HH:mm:ss"
+		strftime(tim_str, sizeof(tim_str), "%Y-%m-%d %H:%M:%S", &t);
+		tim_fmt = tim_str;
+	}
+	return tim_fmt.c_str();
+}
+
+uint64_t Timer::GetTimeMicroSecond()
+{
+	timeb t;
+	ftime(&t); // Get ms time
+	return t.time*1000 + t.millitm;
+}
+
+NS_CU_END
+
+#pragma optimize("", on)
--- app/src/kits/CUtils/CUTimer.h
+++ app/src/kits/CUtils/CUTimer.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,11 +20,32 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef CUTILS_H
-#define CUTILS_H
+// https://blog.csdn.net/weixin_45525272/article/details/109687419
 
-#include <CUtils/CUString.h>
-#include <CUtils/CUFile.h>
-#include <CUtils/CUTimer.h>
+#ifndef CU_TIMER_H
+#define CU_TIMER_H
 
-#endif /* CUTILS_H */
+#include <stdint.h>
+#include "CUtilsDef.h"
+
+NS_CU_BEGIN
+namespace Timer {
+
+	/// @breif Get Time In Second
+	uint64_t GetTimeSecond();
+
+	/// @breif second to string time. e.g. 1699158041 to 2023-11-05 12:20:41
+	/// @param timestamp	input timestamp in second. e.g. 1699158041
+	/// @param format		output date format. supports: 
+	///		YYYY-MM-DD HH:mm:ss		YYYYMMDD-HHmmss
+	/// @return timestamp in target fmt.
+	const char* SecondTime2Str(uint64_t timestamp, const char* format = "YYYY-MM-DD HH:mm:ss");
+
+	/// @breif Get Time In Micro-Second
+	uint64_t GetTimeMicroSecond();
+
+} // namespace Timer
+NS_CU_END
+
+
+#endif // CU_TIMER_H
