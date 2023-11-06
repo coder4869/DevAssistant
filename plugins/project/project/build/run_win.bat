f98617fbff94109a92a38b1b60da13657bb05ce1
coder4869@gmial.com <158755love>
2023-11-07 07:47:44 +0800

    update License

--- plugins/project/project/build/run_win.bat
+++ plugins/project/project/build/run_win.bat
@@ -1,6 +1,6 @@
 @REM MIT License
 @REM 
-@REM Copyright (c) 2021~2022 [coder4869](https://github.com/coder4869)
+@REM Copyright (c) 2021~2023 [coder4869](https://github.com/coder4869)
 @REM 
 @REM Permission is hereby granted, free of charge, to any person obtaining a copy
 @REM of this software and associated documentation files (the "Software"), to deal
