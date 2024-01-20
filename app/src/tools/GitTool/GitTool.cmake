7e8f0087d0a586ba970aa556721f81bdb8a7daa5
coder4869 <coder4869@gmail.com>
2024-01-20 12:27:39 +0800

    init commit GitTool Module

---
+++ app/src/tools/GitTool/GitTool.cmake
@@ -0,0 +1,65 @@
+# MIT License
+# 
+# Copyright (c) 2021~2023 [coder4869](https://github.com/coder4869)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set(GitToolDir ${CMAKE_CURRENT_LIST_DIR})
+set(LIB_NAME GitTool)
+
+FILE(GLOB_RECURSE GitTool_SRC
+    ${GitToolDir}/*.h
+    ${GitToolDir}/*.hpp
+
+    ${GitToolDir}/*.c
+    ${GitToolDir}/*.cc
+    ${GitToolDir}/*.cpp
+    )
+
+if(NOT ANDROID)
+    source_group(
+        TREE ${GitToolDir}
+        PREFIX "GitTool"
+        FILES ${GitTool_SRC}
+        )
+endif(NOT ANDROID)
+
+set(LIB_DEPS ${THIRD_PARTY_LIB} )
+
+add_library(${LIB_NAME} ${LIB_TYPE} ${GitTool_SRC})
+set_target_properties(${LIB_NAME} PROPERTIES FOLDER "tools")
+target_compile_definitions(${LIB_NAME} PRIVATE GITTOOL_EXPORT )
+target_include_directories(${LIB_NAME} PRIVATE ${INC_PY} ${GitToolDir} ${INC_GROUP} )
+target_link_directories(${LIB_NAME} PUBLIC ${LIB_LINK_DIR})
+target_link_libraries(${LIB_NAME} ${LIB_PY} ${LIB_DEPS} )
+
+if(WITH_PY)
+    target_compile_definitions(${LIB_NAME} PRIVATE WITH_PY )
+endif(WITH_PY)
+
+# install libs & headers
+INSTALL_INC(${CMAKE_CURRENT_LIST_DIR} include/)
+INSTALL_TARGET(${LIB_NAME}) # lib bin exe
+
+if(WIN)
+    set_target_properties(${LIB_NAME} PROPERTIES COMPILE_FLAGS "/EHsc")
+elseif(APPLE)
+    # from intern cmake module : apple_func.cmake
+    XCODE_SETTING(${LIB_NAME} ${OS_MIN_VERSION})
+endif(APPLE)
---
+++ app/src/tools/GitTool/GitToolDef.h
@@ -0,0 +1,51 @@
+﻿// MIT License
+//
+// Copyright (c) 2021~2023 [coder4869](https://github.com/coder4869)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef GITTOOL_DEF_H
+#define GITTOOL_DEF_H
+
+#define NS_GIT_BEGIN namespace GIT {
+#define NS_GIT_END	} //namespace GIT
+
+#ifdef GITTOOL_EXPORT
+
+#	if (defined WIN) || (defined WIN32) || (defined _WIN32) || (defined _WIN64)
+#		define GITTOOL_CLASS __declspec(dllexport)
+#		define GITTOOL_API extern "C" __declspec(dllexport)
+#	else
+#		define GITTOOL_CLASS __attribute__((externally_visible))
+#		define GITTOOL_API extern "C" __attribute__((externally_visible))
+#	endif
+
+#else
+
+#	if (defined WIN) || (defined WIN32) || (defined _WIN32) || (defined _WIN64)
+#		define GITTOOL_CLASS __declspec(dllimport)
+#		define GITTOOL_API extern "C" __declspec(dllimport)
+#	else
+#		define GITTOOL_CLASS __attribute__((externally_visible))
+#		define GITTOOL_API extern "C" __attribute__((externally_visible))
+#	endif
+
+#endif // GITTOOL_EXPORT
+
+#endif /* GITTOOL_DEF_H */
