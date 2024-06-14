dcd6082d500756554274fe2116b86d8a43bec4e0
coder4869 <coder4869@gmail.com>
2024-06-14 21:42:39 +0800

    update gradle plugin

--- plugins/templates/android/plugin/src/main/groovy/dev/coder4869/gplugin/PickLatestPlugin.groovy
+++ plugins/templates/android/plugin/src/main/groovy/dev/coder4869/gplugin/PickLatestPlugin.groovy
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2021~2024 [coder4869](https://github.com/coder4869)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Usage: in target project add following line 
 package dev.coder4869.gplugin
 
@@ -318,11 +340,15 @@ class GradleDepends {
     public static void showConfigurationInfo(Configuration cfg) {
         println cfg.getIncoming().toString()
         Configuration cfgCopy = cfg.copyRecursive()
-        cfgCopy.setCanBeResolved(true)
-        println("ConfigurationFiles = " + cfgCopy.getFiles().toString())
+        try {
+            cfgCopy.setCanBeResolved(true)
+            println("ConfigurationFiles = " + cfgCopy.getFiles().toString())
 
-        ResolvedConfiguration resolvedCfg = cfgCopy.getResolvedConfiguration()
-        println("ResolvedConfiguration = " + resolvedCfg.getFiles().toString())
+            ResolvedConfiguration resolvedCfg = cfgCopy.getResolvedConfiguration()
+            println("ResolvedConfiguration = " + resolvedCfg.getFiles().toString())
+        } catch (Exception exc) {
+            println("showConfigurationInfo error = " + exc.getMessage())
+        }
     }
 
     public static Set<File> getConfigurationDependencies(Configuration cfg,
@@ -334,24 +360,11 @@ class GradleDepends {
 
         Set<File> depends = []
         Configuration cfgCopy = cfg.copyRecursive()
-        cfgCopy.setCanBeResolved(true)
-        if (isAll) {
-            for (final File f in cfgCopy.getFiles()) {
-                if (f.toString().endsWith(suffix)) {
-                    int size = depends.size()
-                    depends.add(f)
-                    if (depends.size() > size) {
-                        println f.toString()
-                    }
-                }
-            }
-        } else { // Only first level
-            for (final Dependency dep in cfgCopy.getDependencies()) {
-                def dep_str = dep.group + '/' + dep.name + '/' + dep.version
-                println dep_str
-
+        try {
+            cfgCopy.setCanBeResolved(true)
+            if (isAll) {
                 for (final File f in cfgCopy.getFiles()) {
-                    if (f.toString().contains(dep_str)) {
+                    if (f.toString().endsWith(suffix)) {
                         int size = depends.size()
                         depends.add(f)
                         if (depends.size() > size) {
@@ -359,9 +372,25 @@ class GradleDepends {
                         }
                     }
                 }
-            }
-        }
+            } else { // Only first level
+                for (final Dependency dep in cfgCopy.getDependencies()) {
+                    def dep_str = dep.group + '/' + dep.name + '/' + dep.version
+                    println dep_str
 
+                    for (final File f in cfgCopy.getFiles()) {
+                        if (f.toString().contains(dep_str)) {
+                            int size = depends.size()
+                            depends.add(f)
+                            if (depends.size() > size) {
+                                println f.toString()
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception exc) {
+            println("error = " + exc.getMessage())
+        }
         println("getConfigurationDependencies end !")
         return depends
     }
@@ -399,7 +428,7 @@ class GradleDepends {
 
         Set<File> fileSet = getTypedDependencies("api", suffix, isAll)
         Set<File> implSet = getTypedDependencies("implementation", suffix, isAll)
-        implSet.forEach { fileSet.add(it) }
+        fileSet.addAll(implSet)
 
         println("getDependencies end !")
         return fileSet
@@ -455,3 +484,5 @@ public class PickLatestPlugin implements Plugin<Project> {
         }
     }
 }
+
+//apply plugin: PickLatestPlugin
